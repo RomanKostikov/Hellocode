8713282eb6c065c9e7c06940b443525e9ad9a7c2
Roman <romankos1993@gmail.com>
2022-10-02 18:18:15 +0300

    Example 011 ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿// Заполнить массив случайными числами и вывести их на экран(используя методы).
+void FillArray(int[] collection) 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10];
+
+FillArray(array); // вызвал метод заполнения массива
+PrintArray(array); // метод вывода на экран
