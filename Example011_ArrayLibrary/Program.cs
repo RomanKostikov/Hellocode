1b5ac5d0743483b482ebe4641819361b68f6df10
Roman <romankos1993@gmail.com>
2022-10-02 18:59:19 +0300

    Example011_ArrayLibrary + решенная задача с find

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,5 +1,5 @@
-﻿// Заполнить массив случайными числами и вывести их на экран(используя методы).
-void FillArray(int[] collection) 
+﻿// a) Заполнить массив случайными числами и вывести их на экран(используя методы).
+void FillArray(int[] collection)
 {
     int length = collection.Length;
     int index = 0;
@@ -19,7 +19,31 @@ void PrintArray(int[] col)
         position++;
     }
 }
+//b) Решим задачу с find при помощи этих методов.
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // специальный прием для случаев, которые не входят в диапазон массива
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        //index = index + 1;
+        index++;
+    }
+    return position;
+}
 int[] array = new int[10];
 
 FillArray(array); // вызвал метод заполнения массива
+array[4] = 4; //исскуствено добавили еще по 4ке
+array[7] = 4;
 PrintArray(array); // метод вывода на экран
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
