08714d3b2b5dee9eee624704e9523af5beb5e216
Roman <romankos1993@gmail.com>
2022-10-20 18:08:12 +0300

    Example 014 completed

---
+++ Example014_Exotica/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Случай, когда не известен наперед тип данных
+// int a = 12;
+using System.Linq;
+
+var a = 12; //сам определил тип данных через var
+// Console.WriteLine(a.GetType());
+
+var data = new int[] { 1, 2, 3, 4 }
+         .Where(e => e > 0)
+         .Select(e => new { q = e, w = e + 1 });
+Console.WriteLine(data.GetType().Name);
+// 
